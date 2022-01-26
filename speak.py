7fed5b55f57f864f4811557768c9a408d0731f02
Jonathan Bangert <jonathan@bangert.dk>
2022-01-26 09:47:51 +0100

    Eo

--- speak.py
+++ speak.py
@@ -19,7 +19,7 @@ if not exists(dirr):
 
 #Say function
 def say(text):
-    if not text:
+    if not text or ')' in text or '(' in text:
         return
     #Convert text to hash for file name
     filename = md5(text.encode()).hexdigest()
