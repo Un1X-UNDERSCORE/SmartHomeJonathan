96f507fb6830e9ddb1a26d44cf78562894b98d54
Jonathan <jonathan@bangert.dk>
2022-01-30 16:30:12 +0100

    Lmao

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+# Imports
 import cores.speak as speak
 import neuralintents
 from cores.coremodules import core
 
+# Mappings
 mappings = {'openminecraft' : core.minecraft, 'addnote':core.addnote, 'removenote':core.removenote, 'listnotes':core.listnotes}
+# Define Neural Networks
 n = neuralintents.GenericAssistant('intents.json', intent_methods=mappings)
 
+# Load Neural Network
 n.load_model()
 
+# Assistant Loop
 while True:
+    # Input
     text = input('Say something: ').lower()
+    # Say Response
     speak.say(n.request(text))
