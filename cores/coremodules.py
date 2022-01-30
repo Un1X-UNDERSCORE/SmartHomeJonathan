838b911ae0777ff483d374f557ff7be61498cd17
Jonathan <jonathan@bangert.dk>
2022-01-30 21:16:02 +0100

    I am verry tired

--- cores/coremodules.py
+++ cores/coremodules.py
@@ -3,15 +3,20 @@ from cores.speak import say
 import os
 from threading import Thread
 
+# Make core class
 class core:
+    # Add a note func
     def addnote():
         say('what do you want to add to your notes? ')
         note = input().lower()
         note = note.strip('\n')
+        # Open the text file
         with open('notes.txt', 'a') as f:
             f.write(f'\n{note}')
         say(f'Added {note} to your notes!')
+    # List notes func
     def listnotes():
+        # Open the text file
         with open('notes.txt', 'r+') as f:
             lines = f.readlines()
             bruh = []
@@ -20,9 +25,12 @@ class core:
             say('Your notes is as following:')
             for aaa in bruh:
                 say(aaa)
+    # Remove a note func
     def removenote():
+        # Get the notes that has to get removed
         say('what do you want to remove from notes? ')
         note = input().lower()
+        # Open the text file
         with open('notes.txt', 'r+') as f:
             lines = f.readlines()
             file = open('notes.txt', 'w')
